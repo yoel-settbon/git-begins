1a65287c465d37bc977739fdfb1f35cc0d5716d8
yoel-settbon <yoel-settbon@laplateforme.io>
2024-10-21 15:13:08 +0200

    commands-git

---
+++ commands-git.py
@@ -0,0 +1,2 @@
+"""Pour cloner un repository, utiliser la fonction git clone suivi du lien du repository préalablement copié sur github.
+Les trois commandes de push sont : "git add .", "git commit -m", et "git push ."""
